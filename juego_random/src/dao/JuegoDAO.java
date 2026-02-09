fa6fe3f275cf99c8fdf1f6920cd65f657744ffab
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2026-02-09 12:12:01 -0500

    Add files via upload

--- juego_random/src/dao/JuegoDAO.java
+++ juego_random/src/dao/JuegoDAO.java
@@ -1,4 +1,30 @@
 package dao;
 
+import util.DBConnection;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+
 public class JuegoDAO {
+
+    public void guardarJuego(
+            int numSecreto,
+            int numJugador,
+            int intentos,
+            String resultado
+    ) throws Exception {
+
+        Connection con = DBConnection.getConnection();
+        CallableStatement cs = con.prepareCall(
+                "{ call SP_GUARDAR_JUEGO(?,?,?,?) }"
+        );
+
+        cs.setInt(1, numSecreto);
+        cs.setInt(2, numJugador);
+        cs.setInt(3, intentos);
+        cs.setString(4, resultado);
+
+        cs.execute();
+        cs.close();
+        con.close();
+    }   
 }
